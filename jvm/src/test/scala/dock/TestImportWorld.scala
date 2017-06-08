1f52be88c54cf98a1eada8b509e748233bc40671
TheBizzle <jason.bertsche@gmail.com>
2017-06-08 11:58:44 -0500

    Test: Check importing with Climate Change

--- jvm/src/test/scala/dock/TestImportWorld.scala
+++ jvm/src/test/scala/dock/TestImportWorld.scala
@@ -20,6 +20,16 @@ class TestImportWorld extends DockingSuite {
     testCommand(s"""import-world "${csvPath("pd-two-person-iterated")}"""")
   }
 
+  test("Import Climate Change - Fresh", SlowTest) { implicit fixture => import fixture._
+    open("models/Sample Models/Earth Science/Climate Change.nlogo", None)
+    testCommand(s"""import-world "${csvPath("climate-change")}"""")
+  }
+
+  test("Import Climate Change - Clobber", SlowTest) { implicit fixture => import fixture._
+    open("models/Sample Models/Earth Science/Climate Change.nlogo", None)
+    testCommand("setup")
+    testCommand(s"""import-world "${csvPath("climate-change")}"""")
+  }
 
   private def csvPath(name: String): String =
     s"${Paths.get("").toAbsolutePath}/resources/test/export-world/$name.csv"
